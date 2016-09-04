8ad1be8a6805dd71bc9eb0b3501a3d7bb66598a9
Sandeep Shinde <sandeep.shinde@example.com>
2016-09-05 00:10:13 +0530

    Second commit

--- src/main/java/org/rest/jacksontest/JacksonTester.java
+++ src/main/java/org/rest/jacksontest/JacksonTester.java
@@ -2,9 +2,12 @@ package org.rest.jacksontest;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -12,31 +15,58 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class JacksonTester {
    public static void main(String args[]){
       JacksonTester tester = new JacksonTester();
-		
       try {
+         ObjectMapper mapper = new ObjectMapper();
+
+         Map<String, UserData> userDataMap = new HashMap<String, UserData>();
+         UserData studentData = new UserData(); 
+         int[] marks = {1,2,3};
+
          Student student = new Student();
          student.setAge(10);
          student.setName("Mahesh");
-         tester.writeJSON(student);
+			
+         // JAVA Object
+         studentData.setStudent(student);
+			
+         // JAVA String
+         studentData.setName("Mahesh Kumar");
+			
+         // JAVA Boolean
+         studentData.setVerified(Boolean.FALSE);
+			
+         // Array
+         studentData.setMarks(marks);
+         TypeReference ref = new TypeReference<Map<String,UserData>>() { };
+         
+         userDataMap.put("studentData1", studentData);
+         mapper.writeValue( new File("student.json"), userDataMap );
+			
+         // {
+         //    "studentData1":
+         //    {
+         //       "student":
+         //        {
+         //           "name":"Mahesh",
+         //           "age":10
+         //        },
+         //    "name":"Mahesh Kumar",
+         //    "verified":false,
+         //    "marks":[1,2,3]
+         //    }
+         // }
+         
+         userDataMap = mapper.readValue(new File("student.json"), ref);
 
-         Student student1 = tester.readJSON();
-         System.out.println(student1);
-
-      } 
-      catch (JsonParseException e) { e.printStackTrace(); }
-      catch (JsonMappingException e) { e.printStackTrace(); }
+         System.out.println( userDataMap.get("studentData1").getStudent() );
+         System.out.println( userDataMap.get("studentData1").getName() );
+         System.out.println( userDataMap.get("studentData1").getVerified() );
+         System.out.println( Arrays.toString(userDataMap.get("studentData1").getMarks()) );
+			
+      }
+      catch (JsonParseException e) { e.printStackTrace(); } 
+      catch (JsonMappingException e) { e.printStackTrace(); } 
       catch (IOException e) { e.printStackTrace(); }
-   }
-
-   private void writeJSON(Student student) throws JsonGenerationException, JsonMappingException, IOException{
-      ObjectMapper mapper = new ObjectMapper();	
-      mapper.writeValue(new File("student.json"), student);
-   }
-
-   private Student readJSON() throws JsonParseException, JsonMappingException, IOException{
-      ObjectMapper mapper = new ObjectMapper();
-      Student student = mapper.readValue(new File("student.json"), Student.class);
-      return student;
    }
 }
 
@@ -65,4 +95,43 @@ class Student {
    public String toString(){
       return "Student [ name: "+name+", age: "+ age+ " ]";
    }	
+}
+
+class UserData {
+   private Student student;
+   private String name;
+   private Boolean verified;
+   private int[] marks;
+
+   public UserData(){}
+
+   public Student getStudent() {
+      return student;
+   }
+	
+   public void setStudent(Student student) {
+      this.student = student;
+   }
+	
+   public String getName() {
+      return name;
+   }
+   public void setName(String name) {
+      this.name = name;
+   }
+	
+   public Boolean getVerified() {
+      return verified;
+   }
+	
+   public void setVerified(Boolean verified) {
+      this.verified = verified;
+   }
+   public int[] getMarks() {
+      return marks;
+   }
+	
+   public void setMarks(int[] marks) {
+      this.marks = marks;
+   }		
 }
